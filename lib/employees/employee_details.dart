5a4b3999fb0904f1e3d081fbd93c0c104059e8ea
Juic3 <35728870+DJuic3@users.noreply.github.com>
2024-01-26 09:55:01 +0200

    Commit for meeting cart error fixed

--- lib/employees/employee_details.dart
+++ lib/employees/employee_details.dart
@@ -12,6 +12,7 @@ import '../contributions/landingpage.dart';
 import '../contributions/mycontributions.dart';
 import '../contributions/view.dart';
 import '../others/leave_page.dart';
+import '../others/meeting_cart.dart';
 import '../profile/profile.dart';
 import '../store_data/accouncontroller.dart';
 import '../store_data/ui.dart';
